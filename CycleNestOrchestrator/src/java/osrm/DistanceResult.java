7f28fb2fdfe4ca386d0f5b22d29b88a9f9e78933
Eden Hendry <N1085361@my.ntu.ac.uk>
2025-11-14 21:50:48 +0000

    Created OsrmResponse model to map OSRM JSON structure. Added DistanceResult DTO to return processed distance/duration values to clients. Ensures separation of external API schema from internal response model.

---
+++ CycleNestOrchestrator/src/java/osrm/DistanceResult.java
@@ -0,0 +1,11 @@
+package osrm;
+
+public class DistanceResult {
+    public double distance;
+    public double duration;
+
+    public DistanceResult(double distance, double duration) {
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
