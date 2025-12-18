21abb8b5a04493d8dbd76b7b6f974ab6362ad045
Eden Hendry <N1085361@my.ntu.ac.uk>
2025-12-18 22:52:15 +0000

    Implemented custom OsrmException with ErrorType enum for strong QoS reporting

--- CycleNestOrchestrator/src/java/osrm/OsrmException.java
+++ CycleNestOrchestrator/src/java/osrm/OsrmException.java
@@ -1,12 +1,30 @@
 package osrm;
 
+/**
+ * Enhanced Exception for OSRM operations.
+ * Includes error codes to differentiate between client and server issues (QoS).
+ */
 public class OsrmException extends Exception {
 
-    public OsrmException(String message) {
-        super(message);
+    public enum ErrorType {
+        INVALID_COORDINATES,
+        SERVICE_UNAVAILABLE,
+        UNEXPECTED_ERROR
     }
 
-    public OsrmException(String message, Throwable cause) {
+    private final ErrorType errorType;
+
+    public OsrmException(String message, ErrorType errorType) {
+        super(message);
+        this.errorType = errorType;
+    }
+
+    public OsrmException(String message, ErrorType errorType, Throwable cause) {
         super(message, cause);
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
     }
 }
